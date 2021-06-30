ae96d4a74edb758cc960d2665afa8b1b99377970
Pascale <deshaye.p@gmail.com>
2021-06-30 15:32:56 -0400

    Yo

---
+++ src/test/java/com/pascale/testjenkins2/NewMainTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pascale.testjenkins2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pascale
+ */
+public class NewMainTest {
+    
+    public NewMainTest() {
+    }
+//
+//    @org.junit.BeforeClass
+//    public static void setUpClass() throws Exception {
+//    }
+//
+//    @org.junit.AfterClass
+//    public static void tearDownClass() throws Exception {
+//    }
+//
+//    @org.junit.Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @org.junit.After
+//    public void tearDown() throws Exception {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+
+    /**
+     * Test of main method, of class NewMain.
+     */
+    @org.junit.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        NewMain.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
